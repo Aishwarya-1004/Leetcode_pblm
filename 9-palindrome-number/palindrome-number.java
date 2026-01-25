3c6b79e7601c6c3fc7ea832061989870c332de4a
Aishwarya K <167102179+Aishwarya-1004@users.noreply.github.com>
2026-01-25 23:44:24 +0530

    Time: 4 ms (99.69%) | Memory: 46 MB (43.63%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0 || x % 10 == 0 && x!=0){
+            return false;
+        }
+        int reversedhalf = 0;
+        while(x > reversedhalf){
+            int lastdigit = x%10;
+            reversedhalf = reversedhalf*10+lastdigit;
+            x=x/10;
+        }
+        return x == reversedhalf || x== reversedhalf/10;
+    }
+}
